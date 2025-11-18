69fe89278b2d11a2ad5a504e0a2b249b4a6b2a52
Notebook <maxprizrac@mail.ru>
2025-11-18 12:40:28 +0300

    ADD Лаба_11_Алгоримт

--- Алгоритмизация ЛАБЫ/Лаба_11_Алгоритм/11.pas
+++ Алгоритмизация ЛАБЫ/Лаба_11_Алгоритм/11.pas
@@ -58,31 +58,122 @@ begin
   LineTo(315,150);
   LineTo(300,200);
   LineTo(200,200);
-  //FloodFill(210,190,clBlue);
+  FloodFill(210,190,clBlue);
+  
+  Circle(130,100,30);
+  FloodFill(210,190,clBlue);
   
   
   //Треугольник Правый
-  MoveTo(350,200);
+  MoveTo(400,200);
   LineTo(500,200);
   LineTo(570,100);
-  LineTo(350,200);
-  //FloodFill(500,190,clLime);
+  LineTo(385,150);
+  FloodFill(450,150,clWhite);
+  
+  Circle(570,100,30);
+  FloodFill(570,100,clWhite);
   
   //Треугольник Центральный
   MoveTo(300,200);
   LineTo(350,20);
   LineTo(400,200);
-  LineTo(350,200);
-  //FloodFill(350,190,clRed);
-  Circle(390,190,1);
- 
+  LineTo(300,200);
+  FloodFill(350,190,clRed);
   
-  
+  Circle(350,20,30);
+  FloodFill(350,20,clRed);
 end;
+
+
+procedure task3;
+begin
+  
+  var x:=50;
+  
+  while x <= 290 do 
+    begin
+      Circle(x, 100, 10);
+      FloodFill(x, 100, RGB(random(256), random(256), random(256)));
+      x:= x + 30;
+    end;  
+end;
+
+
+procedure task4;
+begin
+
+for var i:= 0 to 1000 do
+  begin
+    Circle(200, 200, Random(100))
+  end;
+end;
+
+
+procedure task5;
+begin
+  for var i:= 0 to 10 do
+    begin
+      SetBrushColor(RGB(random(256), random(256), random(256)));
+      Circle(i*50, i*40, i*5);
+    end;
+end;  
+
+
+procedure task6;
+begin
+  var x:=windowwidth div 10;
+  var y:=windowheight div 10;
+
+  for var i := 0 to 10 do 
+  begin
+    for var j:= 0 to 10 do
+    begin
+      if (i + j) mod 2 = 0 then setbrushcolor(clBlack)
+      else setbrushcolor(clWhite);
+      Rectangle(i*x,j*y,(i+1)*x,(j+1)*y)
+    end;
+  end;
+end;
+
+
+procedure task7;
+begin  
+  var x :=56;
+  var y :=360;
+  setbrushcolor(clBlack);
+  Rectangle(36,360,76,400);
+  
+ repeat 
+  setpencolor(clWhite);
+  circle(x,y,1);
+  setpencolor(clGray);
+  circle(x,y,1);
+  x:=x+1;
+  y:=y-1;
+ until x>windowwidth div 2;
+ repeat 
+  setpencolor(clWhite);
+  circle(x,y,1);
+  setpencolor(clGray);
+  circle(x,y,1);
+  x:=x+1;
+  y:=y+1;
+ until x>600;
+ setpencolor(clGray);
+ setbrushcolor(clGray);
+ Rectangle(570,360,610,400);
+end;
+
 
 Begin
   //task0;
   //task1;
-  task2
+  //task2;
+  //task3;
+  //task4;
+  //task5;
+  //task6;
+  //task7
   
 End.
